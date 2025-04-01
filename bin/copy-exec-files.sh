9899c01b556528d87ce666b3f79e13bd6d7261c6
Antonis Katsarakis <antoniskatsarakis@yahoo.com>
2025-04-01 06:26:10 +0200

    Hermes with ALRs

---
+++ bin/copy-exec-files.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+FILES_TO_CPY=(
+        "hosts.sh"
+        "run.sh"
+        "run-hermesKV.sh"
+        "hermesKV"
+#        "hades"
+#        "run-hades.sh"
+      )
+
+EXEC_FOLDER="${HOME}/hermes/exec"
+
+cd $EXEC_FOLDER
+# get Hosts
+source ../exec/hosts.sh
+make clean; make
+cd -
+
+for FILE in "${FILES_TO_CPY[@]}"
+do
+	parallel scp ${EXEC_FOLDER}/${FILE} {}:${EXEC_FOLDER}/${FILE} ::: $(echo ${REMOTE_HOSTS[@]})
+	echo "${FILE} copied to {${REMOTE_HOSTS[@]}}"
+done
+
